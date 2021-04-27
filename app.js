80aa63450e2dc5f1b3328a20094809dfaf6d8877
Nicolas <nicobrazzo@gmail.com>
2021-04-27 22:10:30 +0200

    ADD CORS

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(passport.initialize());
+app.use(cors());
 
 app.use('/api', accountRouter);
 
